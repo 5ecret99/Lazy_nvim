52738989e6e9d4bb29f07c97c6ef214c49752974
gigi <georgiougeorgexristou@gmail.com>
2024-10-18 20:33:06 +0300

    fixed explorer issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,25 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap('n', '<C-f>', "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-g>', "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-b>', "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-u>', "<cmd>Telescope resume<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-e>', "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-f>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-b>", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<cmd>Telescope resume<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>Neotree<CR>", { noremap = true, silent = true })
 
-vim.keymap.set('n', ';', '<Nop>')  -- Unbind semicolon from its default behavior
+vim.keymap.set("n", ";", "<Nop>") -- Unbind semicolon from its default behavior
 
 local wk = require("which-key")
 
 wk.add({
   -- Normal mode mappings
-  { ";", group = "Utils"}, -- Start of Utils menu
-    { ";f", "<cmd>lua vim.lsp.buf.format()<CR>", desc = "Format Document"},
-    { ";a", "<cmd>ClangdSwitchSourceHeader<CR>", desc = "Switch Source/Header"},
-    { ";s", group = "Search"}, -- Start of Search submenu
-      { ";sf", "<cmd>Telescope find_files<CR>", desc = "Find Files"},
-      { ";sg", "<cmd>Telescope live_grep<CR>", desc = "Grep Text"},
-      { ";sb", "<cmd>Telescope buffers<CR>", desc = "Opened Buffers"},
-      { ";se", "<cmd>NvimTreeToggle<CR>", desc = "File Explorer"},
-}, { mode = "n" })  -- Ensure that the mappings are for normal mode only
-
+  { ";", group = "Utils" }, -- Start of Utils menu
+  { ";f", "<cmd>lua vim.lsp.buf.format()<CR>", desc = "Format Document" },
+  { ";a", "<cmd>ClangdSwitchSourceHeader<CR>", desc = "Switch Source/Header" },
+  { ";s", group = "Search" }, -- Start of Search submenu
+  { ";sf", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+  { ";sg", "<cmd>Telescope live_grep<CR>", desc = "Grep Text" },
+  { ";sb", "<cmd>Telescope buffers<CR>", desc = "Opened Buffers" },
+  { ";se", "<cmd>Neotree<CR>", desc = "File Explorer" },
+}, { mode = "n" }) -- Ensure that the mappings are for normal mode only
