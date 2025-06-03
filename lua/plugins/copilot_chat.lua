0f59884f64a0faedad3e23db9ccb7c331c4930be
ggeorgiou <georgios.georgiou@cognyte.local>
2025-06-03 11:07:20 +0300

    Deleted unnecessary copilot configs

--- lua/plugins/copilot_chat.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  branch = "canary",
-  dependencies = { "zbirenbaum/copilot.lua" },
-  config = function()
-    require("CopilotChat").setup()
-  end,
-}
